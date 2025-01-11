268c5db5f6ae8caebcac7a0d5eca52980d04fe77
FirgiK <firgi10222102@sttcipasung.ac.id>
2025-01-12 02:52:57 +0700

    can you feel my heart.. etetetet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,12 +15,12 @@ class HomeController extends Controller
     return view("home.index", [
         'majors' => [
             [
-                'image' => 'https://source.unsplash.com/1600x900/?computer',
+                'image' => 'https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 'title' => 'Teknik Informatika',
                 'description' => 'Di Jurusan Informatika kamu akan mempelajari berbagai prinsip terkait ilmu komputer mulai dari proses perancangan, pengembangan, pengujian, hingga evaluasi sistem operasi perangkat lunak. Selama kuliah kamu akan banyak mengkaji pemrograman dan komputasi, dan dibekali pula dengan keterampilan merancang perangkat lunak.'
             ],
             [
-                'image' => 'https://source.unsplash.com/1600x900/?industrial',
+                'image' => 'https://plus.unsplash.com/premium_photo-1661492172846-06f14551c039?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 'title' => 'Teknik Industri',
                 'description' => 'Keilmuan Teknik Industri lahir dari keinginan untuk memperbaiki sistem pekerjaan yang sudah ada. Itu dibuktikan dengan salah satu bahasan dalam keilmuan Teknik Industri yang menerapkan Plan, Do, Check, Action.'
             ]
