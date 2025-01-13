042a3ce4286b5e8bbb122365049252f8525ce459
FirgiK <firgi10222102@sttcipasung.ac.id>
2025-01-13 14:12:22 +0700

    oooo kamisamaa

--- resources/views/layanan/upm.blade.php
+++ resources/views/layanan/upm.blade.php
@@ -1,6 +1,40 @@
 <x-layouts.app>
+    <div class="text-[25px] text-center text-[#03824f] font-extrabold mb-2 m-5">
+        <h1>Mahasiswa Help</h1>
+    </div>
+    <div class="container text-xs space-y-5 mx-auto text-center mb-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam totam corporis vitae illum nulla, eaque explicabo saepe ab libero, ipsa quaerat obcaecati inventore ratione nihil laboriosam ducimus impedit debitis? Praesentium?</p>
+    </div>
+    <!-- search component -->
+    <div class="relative mx-auto max-w-xl">
+        <input type="search" name="search" placeholder="Search" 
+            class="w-full h-10 px-4 pr-12 rounded-lg border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm" />
+        <button type="submit" class="absolute right-0 top-0 mt-2 mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5 text-gray-600">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 18l6-6m0 0a8 8 0 10-8 8 8 8 0 008-8z" />
+            </svg>
+        </button>
+    </div>  
+    <!-- Search Results Text dan Konten -->
+    <div class="container space-y-4 mx-auto text-left mb-4">
+        <div class="p-6 space-y-2">
+            <h2 class="text-lg font-semibold border-b-2 border-gray-200 pb-3">Search Results</h2>
+            <div class="space-y-4">
+                <div class="border-b-2 border-gray-200 pb-3">
+                    <a href="#" class="text-md font-medium text-blue-600 hover:underline">Result 1: Example Result Title</a>
+                    <p class="text-sm text-gray-600">This is a description of the first result that matches the search term. It's just a sample content.</p>
+                </div>
 
+                <div class="border-b-2 border-gray-200 pb-3">
+                    <a href="#" class="text-md font-medium text-blue-600 hover:underline">Result 2: Another Example</a>
+                    <p class="text-sm text-gray-600">Here's another example of a result. It provides more details about the second match.</p>
+                </div>
 
-
-
+                <div class="border-b-2 border-gray-200 pb-3">
+                    <a href="#" class="text-md font-medium text-blue-600 hover:underline">Result 3: Third Item</a>
+                    <p class="text-sm text-gray-600">This is the third search result with a short description of the content that fits the query.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layouts.app>
--- resources/views/profile/organisasi.blade.php
+++ resources/views/profile/organisasi.blade.php
@@ -1,3 +1,89 @@
 <x-layouts.app>
-    
+    <div class="text-[25px] text-center text-[#03824f] font-extrabold m-5">
+        <h1>Organisasi</h1>
+    </div>
+    <div class="container mx-auto ">
+        <img class="w-full h-48 object-cover my-3" src="https://media.istockphoto.com/id/1783743147/id/foto/pengusaha-muda-yang-bahagia-berbicara-di-mikrofon-saat-menghadiri-acara-pendidikan-di-aula.jpg?s=1024x1024&w=is&k=20&c=g4DvjUB-z9h3ZTj8jQP0o_3kRYdETK1RlbaAloh9Yuc=" alt="gambar seminar">
+    </div>
+
+    <div class="container border bg-green-200 border-gray-300 rounded-lg space-y-5  mx-auto text-left">
+        <div class="p-6 space-y-2">
+            <b class="block font-semibold text-gray-700">Yayasan Pondok Pesantren Cipasung</b>
+            <div class="grid grid-cols-[150px_20px_1fr] gap-x-2">
+                <span>Pembina</span>
+                <span>:</span>
+                <span>KH. Koko Komarudin</span>
+            </div>
+            <div class="grid grid-cols-[150px_20px_1fr] gap-x-2">
+                <span>Pengawas</span>
+                <span>:</span>
+                <span>KH. Ubaidillah Ruhiat</span>
+            </div>
+            <div class="grid grid-cols-[150px_20px_1fr] gap-x-2">
+                <span>Ketua</span>
+                <span>:</span>
+                <span>KH. Drs. Acep Adang Ruhiat, MSi.</span>
+            </div>
+            <div class="grid grid-cols-[150px_20px_1fr] gap-x-2">
+                <span>Sekretaris</span>
+                <span>:</span>
+                <span>KH. Drs. Abdul Chobir, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[150px_20px_1fr] gap-x-2">
+                <span>Bendahara</span>
+                <span>:</span>
+                <span>H. Agus Syaiful Bahri, SPdI.</span>
+            </div>
+        </div>
+
+        <!-- STTC -->
+        <div class="p-6 space-y-2">
+            <b class="block font-semibold text-gray-700">Sekolah Tinggi Teknologi Cipasung</b>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Ketua</span>
+                <span>:</span>
+                <span>Ahmad Zamakhsyari Sidiq, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Wakil Ketua I</span>
+                <span>:</span>
+                <span>Mamik M. Fuadi, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Wakil Ketua II</span>
+                <span>:</span>
+                <span>Mohammad Sabar Jamil, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Wakil Ketua III</span>
+                <span>:</span>
+                <span>Ajeng Sabarini Muslimah, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Ketua LPPM</span>
+                <span>:</span>
+                <span>Ahmad Said, M.Sc.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Ketua Prodi Teknik Industri</span>
+                <span>:</span>
+                <span>Ernawati, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Sekretaris Prodi Teknik Industri</span>
+                <span>:</span>
+                <span>Salim, M.T.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Ketua Prodi Informatika</span>
+                <span>:</span>
+                <span>Mohammad Rijal, S.Kom., M.Sc.</span>
+            </div>
+            <div class="grid grid-cols-[280px_20px_1fr] gap-x-2">
+                <span>Sekretaris Prodi Informatika</span>
+                <span>:</span>
+                <span>Sahara Syarifatul Choeriyah, M.M.</span>
+            </div>
+        </div>
+    </div>
 </x-layouts.app>
